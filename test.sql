c85b73079668320238bd53ee689ead61fdf027d9
sebass0624 <ssan6939@my.msmc.edu>
2025-11-13 11:44:41 -0500

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE monitor_maintenance ( 
+    maintenance_id INT AUTO_INCREMENT PRIMARY KEY, 
+    staff_id INT NOT NULL, 
+    buoy_id INT, 
+    balloon_id INT, 
+    surface_id INT, 
+    maintenance_date DATE NOT NULL, 
+    description VARCHAR(255), 
+    -- Define Foreign Key Constraints 
+    FOREIGN KEY  (buoy_id) REFERENCES buoy_info(buoy_id), 
+    FOREIGN KEY  (balloon_id) REFERENCES balloon_info(balloon_id), 
+    FOREIGN KEY  (surface_id) REFERENCES surface_info(surface_id), 
+    FOREIGN KEY (staff_id) REFERENCES staff(staff_id) 
+);
